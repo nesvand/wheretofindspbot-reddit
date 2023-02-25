abd4236bd7935fbccf70879a28902dd79a99229c
Andrew Nesvadba <andrew.nesvadba@neds.com>
2023-02-25 16:42:32 +1000

    feat: respond to mention

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 import Snoowrap from "snoowrap";
-import { SubmissionStream } from "snoostorm";
+import { CommentStream, SubmissionStream, InboxStream } from "snoostorm";
 
 dotenv.config();
 
@@ -8,24 +8,32 @@ const { CLIENT_ID, CLIENT_SECRET, REDDIT_USER, REDDIT_PASS } = process.env;
 if (!(CLIENT_ID && CLIENT_SECRET && REDDIT_USER && REDDIT_PASS))
 	throw new Error("environment variables not set");
 
-const client = new Snoowrap({
-	userAgent: 'wheretofindsouthparkbot-reddit',
-	clientId: CLIENT_ID,
-	clientSecret: CLIENT_SECRET,
-	username: REDDIT_USER,
-	password: REDDIT_PASS,
-});
-
-const submissions = new SubmissionStream(client, {
-	subreddit: 'testingground4bots',
-	limit: 20,
-	pollTime: 2000,
-});
-
-submissions.on('listing', (l) => {
-	l.forEach((ll) => {
-		if (ll.title.toLowerCase().includes('watch') && ll.title.toLowerCase().includes('where')) {
-			ll.reply('found you');
-		}
+function run() {
+	const client = new Snoowrap({
+		userAgent: "wheretofindsouthparkbot-reddit",
+		clientId: CLIENT_ID,
+		clientSecret: CLIENT_SECRET,
+		username: REDDIT_USER,
+		password: REDDIT_PASS,
 	});
-});
+
+	const inbox = new InboxStream(client, {
+		limit: 20,
+		pollTime: 15_000,
+		filter: 'mentions',
+	});
+
+	try {
+		inbox.on("listing", (listings) => {
+			for (const listing of listings) {
+				if (listing.subreddit_name_prefixed !== 'r/testingground4bots') { continue; }
+				console.log(listing);
+			}
+		});
+	} catch (e) {
+		console.error(e);
+		run();
+	}
+}
+
+run();
